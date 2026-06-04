ff847fd8551539d3384f3a25f60603f0310ff975
hyein89 <bihontai@gmail.com>
2026-06-04 19:28:54 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,33 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'Coming Soon - Project Baru',
-  description: 'Website sedang dalam tahap pengembangan',
+  title: 'Motion Control AI - Pro Generator',
+  description: 'Motion Control AI',
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
+    <html lang="en">
       <head>
-        {/* Memanggil Bootstrap 3 CSS via CDN Resmi */}
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-        />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet" />
       </head>
       <body>
         {children}
+        
+        {/* Script external wajib ditaruh di sini agar bisa diakses oleh page.js */}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
       </body>
     </html>
   );
